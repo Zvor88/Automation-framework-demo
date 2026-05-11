ba209a3028c77f2fee027c0b6caf624ac2109f2f
zbura <zburatorul.g@gmail.com>
2026-05-11 19:01:18 +0300

    confign test

--- src/test/java/tests/APITests.java
+++ src/test/java/tests/APITests.java
@@ -1,6 +1,22 @@
 package tests;
 
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
 public class APITests {
 
+    String baseUri = "https://dogapi.dog/api/v2";
+
+    @Test
+    public void testGetUsersList() {
+        given()
+            .baseUri(baseUri)
+        .when()
+            .get("/breeds")
+        .then()
+            .statusCode(200);
+
+    }
 
 }
